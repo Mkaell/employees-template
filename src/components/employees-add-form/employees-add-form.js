e15e3c9da4b7a7667f523585bff21f5410987a61
Mkaell <mrohacevich@yandex.ru>
2022-01-10 16:21:48 +0300

    The functionality for adding and removing employees has been written

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -16,6 +16,16 @@ class EmployeesAddForm extends Component {
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render(){
         const {name, salary} = this.state;
 
@@ -23,16 +33,20 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
+                        required
+                        minLength={3}
                         name="name"
                         value={name}
                         onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
                         placeholder="З/П в $?"
+                        required
                         name="salary"
                         value={salary}
                         onChange={this.onValueChange}/>
